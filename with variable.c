7ad55bfa93cd476692fb34996b5a880bc4398de1
purvakhant421 <115995434+purvakhant421@users.noreply.github.com>
2023-02-16 19:12:36 +0530

    Add files via upload

---
+++ with variable.c
@@ -0,0 +1,17 @@
+    
+
+#include <stdio.h>
+
+ main()
+{
+    int a=50;
+    int b=20;
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    printf("a=%d",a);
+    printf("b=%d",b);
+
+
+}
---
+++ without.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+ main()
+{
+    int a=10, b=20;      
+    printf("Before swap a=%d b=%d",a,b);      
+    a=a+b;    
+    b=a-b;   
+    a=a-b;   
+    printf("\nAfter swap a=%d b=%d",a,b); 
+
+
+}
